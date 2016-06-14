3939f1c2fc276be575e54603c92b77c531281e2b
David Dorr <shishkebab101@hotmail.com>
2016-06-14 21:06:45 +0300

    Modified two of the view files..
    In particular login_view to make the code simplier to read..

--- application/views/layouts/main.php
+++ application/views/layouts/main.php
@@ -3,9 +3,9 @@
   <head>
     <meta charset="utf-8">
     <title></title>
-    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.min.css">
-    <script src="<?php echo base_url();?>assets/js/jquery.js"></script>
-    <script src="<?php echo base_url();?>assets/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css');?>">
+    <script src="<?php echo base_url('assets/js/jquery.js');?>"></script>
+    <script src="<?php echo base_url('assets/js/bootstrap.min.js');?>"></script>
   </head>
   <body>
     <nav class="navbar navbar-default">
@@ -23,8 +23,8 @@
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
           <ul class="nav navbar-nav">
             <li><a href="<?php echo base_url();?>">Home<span class="sr-only">(current)</span></a></li>
-            <li><a href="<?php echo base_url();?>projects">Projects<span class="sr-only">(current)</span></a></li>
-            <li><a href="<?php echo base_url();?>users/register">Register<span class="sr-only">(current)</span></a></li>
+            <li><a href="<?php echo base_url('projects');?>">Projects<span class="sr-only">(current)</span></a></li>
+            <li><a href="<?php echo base_url('users/register');?>">Register<span class="sr-only">(current)</span></a></li>
           </ul>
           <?php if($this->session->userdata('logged_in')): ?>
           <ul class="nav navbar-nav navbar-right">
@@ -36,10 +36,10 @@
     </nav>
     <div class="container">
       <div class="col-xs-3">
-        <?php $this->load->view('users/login_view'); ?>
+        <?php $this->load->view('users/login_view'); ?> /// the controller should be doing this, with $data being passed to the view
       </div>
       <div class="col-xs-9">
-        <?php $this->load->view($main_view); ?>
+        <?php $this->load->view($main_view); ?> // the controller should be doing this, with $data being passed to the view
       </div>
     </div>
   </body>
